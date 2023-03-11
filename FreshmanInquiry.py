1dd69b447bad916da88d6d99ad6e87d72715c4eb
Cody Jones <Cody.Monroe.Jones@gmail.com>
2023-03-11 12:37:18 -0600

    added title bar to each worksheet

--- FreshmanInquiry.py
+++ FreshmanInquiry.py
@@ -6,7 +6,7 @@ wb = Workbook()
 
 #Creates each sheet in excel workbook we created above
 ws = wb.active #This sheet is a default sheet created when making a new workbook
-ws.title = "IGNORE"  # Declaring the Title for our default worksheek
+ws.title = "IGNORE"
 
 #Titles for our subsequent worksheets
 ws1 = wb.create_sheet("MAE")
@@ -16,6 +16,20 @@ ws4 = wb.create_sheet("IE")
 ws5 = wb.create_sheet("BE")
 ws6 = wb.create_sheet("CE")
 
+#Title Bar for each Column
+for sheet in wb:
+    sheet["A1"] =  "Ambassador Assigned"
+    sheet["B1"] =  "Emailed On"
+    sheet["C1"] =  "Emailed By"
+    sheet["D1"] =  "Comments"
+    sheet["E1"] =  "First Name"
+    sheet["F1"] =  "Preferred Name"
+    sheet["G1"] =  "Last Name"
+    sheet["H1"] =  "Email"
+    sheet["I1"] =  "Start Term"
+    sheet["J1"] =  "Major 1"
+    sheet["K1"] =  "Major 2"
+    sheet["L1"] =  "Major 3"
 
 """
 
